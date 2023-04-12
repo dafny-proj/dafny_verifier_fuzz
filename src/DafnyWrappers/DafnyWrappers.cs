69daa63ecb747360d3e38314175361246bd1bb72
wyt <tee.weiyi22@gmail.com>
2023-04-12 21:44:24 +0100

    [feat] Implement util for displaying Dafny AST structure.

--- src/DafnyWrappers/DafnyWrappers.cs
+++ src/DafnyWrappers/DafnyWrappers.cs
@@ -1,9 +1,8 @@
 ﻿using Dafny = Microsoft.Dafny;
+using System.Diagnostics;
 
 namespace DafnyWrappers;
 public static class DafnyWrappers {
-  public static readonly string[] VerificationArgs = { "verify", "--cores=2", "--use-basename-for-filename", "--verification-time-limit=300" };
-
   public static Dafny.DafnyOptions DefaultDafnyParseOptions() {
     return Dafny.DafnyOptions.Create();
   }
@@ -34,4 +33,58 @@ public static class DafnyWrappers {
     return writer.ToString();
   }
 
+  public const string DefaultDafnyPath = "dafny_proj/dafny/Scripts/dafny";
+  public static readonly string[] DefaultVerificationArgs = {
+    "verify",
+    "--cores=2",
+    "--use-basename-for-filename",
+    "--verification-time-limit=300" };
+
+  public static (int, string, string) RunDafnyVerify(string filepath, string dafnyPath = DefaultDafnyPath) {
+    // Verification API is not easily callable, use Dafny CLI instead
+    var process = new Process();
+    process.StartInfo.FileName = dafnyPath;
+    foreach (var vArg in DefaultVerificationArgs) {
+      process.StartInfo.ArgumentList.Add(vArg);
+    }
+    process.StartInfo.ArgumentList.Add(filepath);
+    process.StartInfo.UseShellExecute = false;
+    process.StartInfo.RedirectStandardInput = true;
+    process.StartInfo.RedirectStandardOutput = true;
+    process.StartInfo.RedirectStandardError = true;
+    process.Start();
+    string output = process.StandardOutput.ReadToEnd();
+    string error = process.StandardError.ReadToEnd();
+    process.WaitForExit();
+    return (process.ExitCode, output, error);
+  }
+
+  public static void PrintDafnyASTByDepth(Dafny.Program programDafny) {
+    List<(string, Dafny.Node)> nodes = new List<(string, Dafny.Node)>();
+    nodes.Add(("0", programDafny));
+    int depth = 0;
+    while (nodes.Count() != 0) {
+      Console.WriteLine(depth);
+      var nextGen = new List<(string, Dafny.Node)>();
+      foreach (var (index, node) in nodes) {
+        Console.WriteLine($"{index}: {node.GetType()}");
+        int count = 1;
+        foreach (var child in node.Children) {
+          nextGen.Add(($"{index}.{count}", child));
+          count++;
+        }
+      }
+      nodes = nextGen;
+      depth++;
+    }
+  }
+
+  public static void PrintDafnyAST(Dafny.Node node, string prefix="0") {
+    Console.WriteLine($"{prefix}: {node.GetType()}");
+    int childCount = 1;
+    foreach (var child in node.Children) {
+      PrintDafnyAST(child, $"{prefix}.{childCount++}");
+    }
+  }
+  
 }
--- src/Main/Experiment.cs
+++ src/Main/Experiment.cs
@@ -1,22 +1,9 @@
-namespace DafnyWrappers;
+using DafnyW = DafnyWrappers.DafnyWrappers;
 
 public class Experiment {
   public static int Main(string[] args) {
-    var programDafny = DafnyWrappers.ParseDafnyProgramFromFile("../../examples/sum.dfy");
-    var prog = AST.Program.FromDafny(programDafny);
-    Console.WriteLine(prog);
-    // var children = programDafny.Children;
-    // int depth = 0;
-    // while (children.Count() != 0) {
-    //   Console.WriteLine(depth);
-    //   var nextGen = new List<Microsoft.Dafny.Node>();
-    //   foreach (var child in children) {
-    //     Console.WriteLine(child.GetType());
-    //     nextGen.AddRange(child.Children);
-    //   }
-    //   children = nextGen;
-    //   depth++;
-    // }
+    var programDafny = DafnyW.ParseDafnyProgramFromFile("../../examples/sum.dfy");
+    DafnyW.PrintDafnyAST(programDafny);
     return 0;
   }
 }
