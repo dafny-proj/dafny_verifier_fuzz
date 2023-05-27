6ed90d02b2ea517ddb549e3ae84104eba7c7afef
wyt <tee.weiyi22@gmail.com>
2023-05-27 18:57:00 +0100

    [feat] Implement simple name generator.

--- src/Fuzzer/Common/Generator.cs
+++ src/Fuzzer/Common/Generator.cs
@@ -5,3 +5,16 @@ public interface IGenerator {
   public string GenVarName();
   public string GenClassName();
 }
+
+public class BasicGenerator : IGenerator {
+  private string id;
+  private int count = 0;
+  public BasicGenerator(string id) {
+    this.id = id;
+  }
+
+  private string GenName(string prefix) => $"{prefix}{count++}_{id}";
+  public string GenClassName() => GenName("C");
+  public string GenLabelName() => GenName("l");
+  public string GenVarName() => GenName("v");
+}
--- tests/Fuzzer.Tests/Utils.cs
+++ tests/Fuzzer.Tests/Utils.cs
@@ -2,18 +2,6 @@ namespace Fuzzer.Tests;
 
 public class MockRandomizer : Randomizer { }
 
-public class MockGenerator : IGenerator {
-  private int count = 0;
-
-  public string GenClassName() {
-    return $"C{count++}_mock";
-  }
-
-  public string GenLabelName() {
-    return $"l{count++}_mock";
-  }
-
-  public string GenVarName() {
-    return $"v{count++}_mock";
-  }
+public class MockGenerator : BasicGenerator {
+  public MockGenerator() : base("mock") { }
 }
